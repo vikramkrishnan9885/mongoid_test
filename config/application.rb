f7e2d92e02ac5c55688611a7b93bd34d3e94b988
Vikram Krishnan <kvikram9885@gmail.com>
2017-09-19 17:57:45 +0530

    Edited configs to make it mongoid usable

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module MongoidTest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Mongoid.load!('./config/mongoid.yml')
+    config.generators {|g| g.orm :mongoid}
   end
 end
